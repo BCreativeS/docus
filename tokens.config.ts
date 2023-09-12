989fc91c6a65cf3e7dba5f37b827c9e3610f493b
AJ Carbs <117075287+BCreativeS@users.noreply.github.com>
2023-09-12 04:40:56 -0700

    H

--- tokens.config.ts
+++ tokens.config.ts
@@ -3,12 +3,18 @@ import { defineTheme } from 'pinceau'
 export default defineTheme({
   color: {
     primary: {
-      '500': '#00DC82'
+      '500': '#00DC82',
+      '100': '#000012'
     },
 
     secondary: {
       '600': '#18181B',
       '900': '#000000'
+    },
+
+    green: {
+      '50': '#fffff7',
+      '600': '#00dc82'
     }
   }
 })
